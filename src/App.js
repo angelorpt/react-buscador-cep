a62b0cee75e658fe7995d837045afb0616d6f575
Angelo Rafael <angelo.peres@contabilizei.com.br>
2022-07-15 13:39:44 -0400

    feat: Implementando a Consulta na API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import { FiSearch } from "react-icons/fi"
-import './styles.css'
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [cep, setCep] = React.useState("");
+
+  const handleSearch = async (event) => {
+    if (!cep) {
+      alert("Preencha um CEP");
+      return;
+    }
+
+    try {
+      const response = await api.get(`/${cep}/json`);
+      console.log(response);
+    } catch (error) {
+      alert("Falha ao consultar o CEP");
+      throw new Error(error);
+    } finally {
+      setCep("");
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu cep" />
-
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite seu cep"
+          value={cep}
+          onChange={({ target }) => setCep(target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="white" />
         </button>
       </div>
@@ -20,7 +45,6 @@ function App() {
         <span>Vila Rosa</span>
         <span>Manaus - AM</span>
       </main>
-
     </div>
   );
 }
